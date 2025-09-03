190cca91f8be5763336598a8325abedcbfc08757
Jmike-dev <gmikekamau@gmail.com>
2025-09-04 01:49:36 +0300

    sevices created all endpoints

--- src/app/services/crops.service.ts
+++ src/app/services/crops.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { BehaviorSubject, tap } from 'rxjs';
+import { Crop } from '@models/farmer.modal';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class CropsService {
+    private apiUrl = environment.apiUrl;
+    private http = inject(HttpClient);
+    private cropsListSubject = new BehaviorSubject<Crop[]>([]);
 
-  constructor() { }
+    constructor() {}
+    createCrop(crop: Crop) {
+        return this.http.post<Crop>(`${this.apiUrl}/crops`, crop);
+    }
+    editCrops(cropId: string, crop: Crop) {
+        return this.http.patch<Crop>(`${this.apiUrl}/crops/${cropId}`, crop);
+    }
+    getCrops(farmerId: string) {
+        return this.http
+            .get<Crop[]>(`${this.apiUrl}/crops/farmer${farmerId}`)
+            .pipe(tap((crops) => this.cropsListSubject.next(crops)));
+    }
 }
