dd1bb63eab8b3f91123993db9645dcfd34690d20
Jmike-dev <gmikekamau@gmail.com>
2025-09-04 13:52:58 +0300

    added logout button

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,17 +1,21 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { MedButtonComponent } from '@components/med-button/med-button.component';
 import {
     Sidebar,
     SideNavComponent,
 } from '@components/side-nav/side-nav.component';
+import { AuthService } from '@services/auth.service';
 
 @Component({
     selector: 'app-dashboard',
-    imports: [RouterOutlet, SideNavComponent],
+    imports: [RouterOutlet, SideNavComponent, MedButtonComponent],
     templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
     // Available routes
+    private authService = inject(AuthService);
+    private router = inject(Router);
     adminDashboardRoutes: Sidebar[] = [
         { name: 'farmers', route: '/admin/farmers', icon: 'personal_injury' },
         { name: 'crops', route: '/admin/crops', icon: 'account_circle' },
@@ -27,4 +31,9 @@ export class DashboardComponent {
 
     // Signal for role from sessionStorage
     role = signal<string | null>(sessionStorage.getItem('role'));
+    logout() {
+        this.authService.logout();
+        this.router.navigate(['/login']);
+        this.role.set(null);
+    }
 }
